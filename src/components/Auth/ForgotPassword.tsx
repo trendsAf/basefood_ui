b8200035b78005bae21995db0fc042675fd239ea
GarrixA <aphrogarrix10@gmail.com>
2024-10-25 08:27:05 +0200

    enh(enhance): reset password enhancement

--- src/components/Auth/ForgotPassword.tsx
+++ src/components/Auth/ForgotPassword.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import { TextField } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { IoMdMail } from "react-icons/io";
 import { ToastContainer, toast } from "react-toastify";
@@ -12,8 +12,9 @@ import { passwordResetRequest } from "../../redux/reducers/auth/passwordResetReq
 const ForgotPassword: React.FC = () => {
   const dispatch = useAppDispatch();
   const { isLoading } = useAppSelector((state) => state.requestPasswordReset);
+  const { control, handleSubmit, reset } = useForm<ResetType>();
 
-  const { control, handleSubmit } = useForm<ResetType>();
+  const [emailSent, setEmailSent] = useState(false);
 
   const onSubmit = async (data: ResetType) => {
     try {
@@ -27,10 +28,9 @@ const ForgotPassword: React.FC = () => {
             result.payload?.message || "Failed to send password reset email.",
           );
         } else {
-          toast.success(
-            result.payload?.message ||
-              "Password reset email sent successfully!",
-          );
+          toast.success(result.payload?.message + " please check your email!");
+          setEmailSent(true);
+          reset();
         }
       } else {
         toast.error("Failed to send password reset email.");
@@ -45,41 +45,51 @@ const ForgotPassword: React.FC = () => {
     <div className="flex flex-col items-center justify-center w-full h-screen">
       <div className="max-w-md w-full p-10 shadow-xl">
         <ToastContainer />
-        <div className="w-full h-full flex flex-col items-center ">
-          <IoMdMail className="text-[5rem] text-blue-600" />
-          <h1 className="text-lg">Please enter your email</h1>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="w-full mx-auto py-4"
-          >
-            <div className="flex flex-col gap-4">
-              <Controller
-                name="email"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    label="Please enter your email"
-                    variant="outlined"
-                    fullWidth
-                    type="email"
-                    className="bg-white"
-                  />
-                )}
-              />
-              <div className="flex justify-center">
-                <button
-                  type="submit"
-                  className="text-white bg-brand-blue px-5 py-3 w-full rounded-[5px] font-bold transition-all duration-300"
-                  disabled={isLoading}
-                >
-                  {isLoading ? "Sending..." : "Send"}
-                </button>
+
+        {emailSent ? (
+          <div className="w-full h-full flex flex-col items-center">
+            <IoMdMail className="text-[5rem] text-green" />
+            <h1 className="text-lg text-center mt-4">
+              Check your email box for the password reset link!
+            </h1>
+          </div>
+        ) : (
+          <div className="w-full h-full flex flex-col items-center">
+            <IoMdMail className="text-[5rem] text-blue-600" />
+            <h1 className="text-lg">Please enter your email</h1>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="w-full mx-auto py-4"
+            >
+              <div className="flex flex-col gap-4">
+                <Controller
+                  name="email"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Please enter your email"
+                      variant="outlined"
+                      fullWidth
+                      type="email"
+                      className="bg-white"
+                    />
+                  )}
+                />
+                <div className="flex justify-center">
+                  <button
+                    type="submit"
+                    className="text-white bg-brand-blue px-5 py-3 w-full rounded-[5px] font-bold transition-all duration-300"
+                    disabled={isLoading}
+                  >
+                    {isLoading ? "Sending..." : "Send"}
+                  </button>
+                </div>
               </div>
-            </div>
-          </form>
-        </div>
+            </form>
+          </div>
+        )}
       </div>
     </div>
   );
