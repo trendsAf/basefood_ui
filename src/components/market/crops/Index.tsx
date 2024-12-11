a19c503640c6b56155e282d7eb62b075e0c8dce5
GarrixA <aphrogarrix10@gmail.com>
2024-12-11 12:28:16 +0200

    feat(guides): initial selection guides

--- src/components/market/crops/Index.tsx
+++ src/components/market/crops/Index.tsx
@@ -9,6 +9,7 @@ import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import { getCropsCategory } from "../../../redux/reducers/crops/cropCategorySlice";
 import { getCrops } from "../../../redux/reducers/crops/cropSlice";
 import { updateField } from "../../../redux/reducers/form/formSlice";
+import { AiOutlineArrowDown } from "react-icons/ai";
 
 const CropSelector: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -27,6 +28,10 @@ const CropSelector: React.FC = () => {
   );
   const { crop_id } = useAppSelector((state) => state.form);
 
+  const [localPrompt, setLocalPrompt] = useState<"category" | "crop" | null>(
+    null,
+  );
+
   useEffect(() => {
     dispatch(getCropsCategory());
     dispatch(getCrops());
@@ -42,29 +47,33 @@ const CropSelector: React.FC = () => {
     }
   }, [selectedCategory, dispatch]);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const cropsMarket = localStorage.getItem("crops_market");
+      const localCategory = localStorage.getItem("selectedCategory");
+      const localCrop = localStorage.getItem("selectedCropId");
+
+      if (!cropsMarket) {
+        if (!localCategory) {
+          setLocalPrompt("category");
+        } else if (!localCrop) {
+          setLocalPrompt("crop");
+        } else {
+          setLocalPrompt(null);
+        }
+      } else {
+        setLocalPrompt(null);
+      }
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
+
   const filteredCrops = selectedCategory
     ? cropList.filter(
         (crop) => crop.crop_category?.toString() === selectedCategory,
       )
     : [];
-
-  useEffect(() => {
-    if (storedCropId) {
-      const storedCrop = cropList.find(
-        (crop) => crop.id.toString() === storedCropId,
-      );
-      if (storedCrop) {
-        const isFromSelectedCategory =
-          storedCrop.crop_category?.toString() === selectedCategory;
-        if (isFromSelectedCategory) {
-          dispatch(updateField({ field: "crop_id", value: storedCropId }));
-        } else {
-          dispatch(updateField({ field: "crop_id", value: "" }));
-          localStorage.removeItem("selectedCropId");
-        }
-      }
-    }
-  }, [storedCropId, selectedCategory, cropList, dispatch]);
 
   useEffect(() => {
     if (crop_id) {
@@ -74,80 +83,131 @@ const CropSelector: React.FC = () => {
 
   const handleCategoryChange = (e: SelectChangeEvent<string>) => {
     setSelectedCategory(e.target.value);
+    setLocalPrompt("crop");
   };
 
   const handleCropChange = (e: SelectChangeEvent<string>) => {
     dispatch(updateField({ field: "crop_id", value: e.target.value }));
+    setLocalPrompt(null);
   };
 
   return (
-    <div className="flex items-center lg:flex-col md:flex-row w-full">
+    <div className="flex items-center lg:flex-col md:flex-row w-full gap-2 pr-2">
       {/* Category Dropdown */}
-      <FormControl
-        sx={{ m: 1, minWidth: 120, width: "100%" }}
-        size="small"
-        disabled={categoryLoading || cropCategoryList.length === 0}
-      >
-        <Select<string>
-          labelId="category-select-label"
-          id="category-select"
-          label="Category"
-          className="!text-sm sm:!text-xl lg:!text-sm xl:!text-lg"
-          value={selectedCategory}
-          onChange={handleCategoryChange}
-          displayEmpty
+      <div className="w-full prompt">
+        {localPrompt === "category" && (
+          <div className="absolute w-full h-full bg-black/70 inset-0 z-50 flex ">
+            <div className="flex gap-2 w-full relative">
+              <div className="flex absolute top-32 left-[20vw]">
+                <AiOutlineArrowDown className="text-white text-2xl animate-bounce" />
+                <span className="text-white text-sm bg-black/80 px-2 py-1 rounded-md ml-4">
+                  Select category
+                </span>
+              </div>
+            </div>
+          </div>
+        )}
+        <FormControl
+          sx={{ m: 1, minWidth: 120, width: "100%" }}
+          size="small"
+          disabled={categoryLoading || cropCategoryList.length === 0}
+          className={`${localPrompt === "category" ? "absolute z-[70] inner" : ""}`}
         >
-          {/* Default "Select Category" Option */}
-          <MenuItem
-            value=""
+          <svg height="100%" width="100%" xmlns="http://www.w3.org/2000/svg">
+            <rect
+              rx="8"
+              ry="8"
+              className={`line ${!localPrompt ? "hidden" : ""}`}
+              height="100%"
+              width="100%"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <Select<string>
+            labelId="category-select-label"
+            id="category-select"
+            label="Category"
             className="!text-sm sm:!text-xl lg:!text-sm xl:!text-lg"
+            value={selectedCategory}
+            onChange={handleCategoryChange}
+            displayEmpty
           >
-            Select Category
-          </MenuItem>
-          {cropCategoryList.map((category) => (
             <MenuItem
-              value={category.id.toString()}
-              key={category.id}
+              value=""
               className="!text-sm sm:!text-xl lg:!text-sm xl:!text-lg"
             >
-              {category.name}
+              Select Category
             </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+            {cropCategoryList.map((category) => (
+              <MenuItem
+                value={category.id.toString()}
+                key={category.id}
+                className="!text-sm sm:!text-xl lg:!text-sm xl:!text-lg"
+              >
+                {category.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
 
       {/* Crops Dropdown */}
-      <FormControl
-        sx={{ m: 1, minWidth: 120, width: "100%" }}
-        size="small"
-        disabled={cropsLoading || !filteredCrops.length || !selectedCategory}
-      >
-        <Select<string>
-          labelId="crop-select-label"
-          id="crop-select"
-          label="Crops"
-          className="!text-sm sm:!text-xl lg:!text-sm xl:!text-lg"
-          value={crop_id || ""}
-          onChange={handleCropChange}
-          displayEmpty
+      <div className="w-full prompt">
+        {localPrompt === "crop" && (
+          <div className="absolute w-full h-full bg-black/70 inset-0 z-50 flex ">
+            <div className="flex gap-2 w-full relative">
+              <div className="flex absolute lg:top-52 lg:left-[20vw] top-36 left-[52vw]">
+                <AiOutlineArrowDown className="text-white text-2xl animate-bounce" />
+                <span className="text-white text-sm bg-black/80 px-2 py-1 rounded-md ml-4">
+                  Select crop
+                </span>
+              </div>
+            </div>
+          </div>
+        )}
+        <FormControl
+          sx={{ m: 1, minWidth: 120, width: "100%" }}
+          size="small"
+          disabled={cropsLoading || !filteredCrops.length || !selectedCategory}
+          className={`${localPrompt === "crop" ? "absolute z-[70] inner" : ""}`}
         >
-          <MenuItem
-            value=""
+          <svg height="100%" width="100%" xmlns="http://www.w3.org/2000/svg">
+            <rect
+              rx="8"
+              ry="8"
+              className={`line ${!localPrompt ? "hidden" : ""}`}
+              height="100%"
+              width="100%"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <Select<string>
+            labelId="crop-select-label"
+            id="crop-select"
+            label="Crops"
             className="!text-sm sm:!text-xl lg:!text-sm xl:!text-lg"
+            value={crop_id || ""}
+            onChange={handleCropChange}
+            displayEmpty
           >
-            Select Crop
-          </MenuItem>
-          {filteredCrops.map((crop) => (
             <MenuItem
-              value={crop.id}
-              key={crop.id}
+              value=""
               className="!text-sm sm:!text-xl lg:!text-sm xl:!text-lg"
             >
-              {crop.name}
+              Select Crop
             </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+            {filteredCrops.map((crop) => (
+              <MenuItem
+                value={crop.id}
+                key={crop.id}
+                className="!text-sm sm:!text-xl lg:!text-sm xl:!text-lg"
+              >
+                {crop.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	darkMode: "class",
-	theme: {
-		extend: {
-			colors: {
-				"white": "#fff",
-				"bg-gray": "#F7F8FA",
-				"dark-gray": "#687588",
-				"brand-blue": "#1456F5",
-				"light-blue": "#EBF1FE",
-				"red": "#ED544E",
-				"light-red": "#F6E8E9",
-				"yellow": "#F4C700",
-				"light-yellow": "#F7F3E1",
-				"green": "#66C87B",
-				"light-green": "#E8F3ED",
-				"black": "#161616",
-				"secondary-black": "#1E1E1E",
-				"transparent": "transparent",
-			  },
-		},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        white: "#fff",
+        "bg-gray": "#F7F8FA",
+        "dark-gray": "#687588",
+        "brand-blue": "#1456F5",
+        "light-blue": "#EBF1FE",
+        red: "#ED544E",
+        "light-red": "#F6E8E9",
+        yellow: "#F4C700",
+        "light-yellow": "#F7F3E1",
+        green: "#66C87B",
+        "light-green": "#E8F3ED",
+        black: "#161616",
+        "secondary-black": "#1E1E1E",
+        transparent: "transparent",
+      },
+    },
+  },
+  plugins: [],
 };
